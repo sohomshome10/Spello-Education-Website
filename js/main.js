21c0b04450ec94ea953e8d539b6dae5520e8bcbd
sohomshome10 <sohom.shome1998@gmail.com>
2021-04-19 19:57:20 +0530

    Commited on 19 Apr 2021

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    // Back to top button.
+    $('body').append('<a href="#home" class="top scroll"><i class="fas fa-chevron-up"></i></a>');
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100){
+            $('.top').fadeIn('slow');
+        }else{
+             $('.top').fadeOut('slow');
+        }
+    });
+    $('.top').click(function(e){
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: 0
+        },500);
+        return false;
+    });
+});
